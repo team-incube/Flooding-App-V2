c59b28137c6d7db489311e6f56293fa722eeb4d9
LeeJueon <loolloo080313@gmail.com>
2026-05-28 11:25:45 +0900

    ♻️ :: main.dart가 LightTheme/DarkTheme 사용하도록 변경 및 미사용 app_theme.dart 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flooding_v2/core/theme/app_theme.dart';
+import 'package:flooding_v2/core/theme/config/light_theme.dart';
+import 'package:flooding_v2/core/theme/config/dark_theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -7,8 +8,8 @@ void main() {
     MaterialApp(
       title: 'flooding_v2',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
+      theme: LightTheme.theme,
+      darkTheme: DarkTheme.theme,
       themeMode: ThemeMode.system,
       home: const HomePage(),
     ),
